57603d1127d19871d56fdb616c24cdf300ab0a58
aymaan <aymaanpathan5@gmail.com>
2026-03-19 20:55:26 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -18,16 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&family=DM+Mono:ital,wght@0,300;0,400;0,500;1,400&display=swap"
-          rel="stylesheet"
-        />
+        <link rel="icon" href="/favicon-new.png" type="image/png" />
       </head>
       <body suppressHydrationWarning>
         <ReduxProvider>{children}</ReduxProvider>
